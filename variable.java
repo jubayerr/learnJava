a1666305ab99a731f8b0206337b1ff7c2abbfd35
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-16 12:13:50 +0600

    variable and data types

---
+++ variable.java
@@ -0,0 +1,17 @@
+// variable and data types
+public class variable {
+    public static void main(String[] args) {
+        // declare variable
+        int randomNumber = 24;
+        double randomNum2 = 2.4;
+        boolean isStudent = true;
+        char character = 'C';
+        String myName = "Jubayer";
+        // output
+        System.out.println(randomNumber);
+        System.out.println(randomNum2);
+        System.out.println(isStudent);
+        System.out.println(character);
+        System.out.println(myName);
+    }
+}
