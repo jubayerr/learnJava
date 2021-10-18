5aa1298d320ffe61e27fe4f0c199ca0092a37fb2
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-18 09:30:18 +0600

    input

---
+++ input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String scanned = sc.next();
+
+        int x = Integer.parseInt(scanned);
+
+        // int scanned2 = sc.nextInt();
+
+        // System.out.println(scanned);
+        // System.out.println(scanned);
+        System.out.println(x);
+    }
+}
