09aa9cce622fae9e05cf8706f26b4595505eeaa9
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-19 09:09:24 +0600

    arrays

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+// Arrays
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        String[] newArr = { "apple", "mango", "banana", "melon" };
+
+        int[] nums = { 2, 3, 4, 5, 6, 8 };
+
+        String x = newArr[0];
+
+        System.out.println(x);
+        System.out.println(nums[0]);
+
+    }
+}
