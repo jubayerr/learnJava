513b78002f72f3579743e503884373b336d072c1
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-20 17:48:18 +0600

    foor loop

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+/**
+ * For Loop
+ */
+public class ForLoop {
+
+    public static void main(String[] args) {
+        int x = 0;
+        for (int i = 0; i <= 10; i++) {
+            x = x + i;
+        }
+        System.out.println(x);
+    }
+}
