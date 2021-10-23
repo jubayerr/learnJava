408e6999bb473eef8ad915d54c316919b96f08ee
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-23 10:52:57 +0600

    object and classes

---
+++ Objectss.java
@@ -0,0 +1,10 @@
+
+/**
+ * Objectss
+ */
+public class Objectss {
+    public static void main(String[] args) {
+        Person p1 = new Person("Jonas", 24);
+        p1.speak();
+    }
+}
---
+++ Person.java
@@ -0,0 +1,14 @@
+// Class
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void speak() {
+        System.out.println("I am " + this.name + " and I am " + this.age + " old.");
+    }
+}
